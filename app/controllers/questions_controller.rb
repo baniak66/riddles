4084a42d2bcb2e5303393d3e3bd66393b3ffefef
Pawel Licznar <pawellicznar@gmail.com>
2016-07-16 21:38:03 +0200

    Update questions index template

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,11 @@
 class QuestionsController < ApplicationController
 
   def index
+    @user = current_user
+    @questions = @user.questions.all
+  end
+
+  def new
+    @question = Question.new
   end
 end
