06b8d9ec5bf8198327a6166b1544a3ea48d0e2a6
Pawel Licznar <pawellicznar@gmail.com>
2016-07-21 00:39:06 +0200

    Eliminate duplication and add column with sum of answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,8 @@ class Question < ApplicationRecord
   has_many :answers
   accepts_nested_attributes_for :answers, allow_destroy: true
 
+  def number_of_answers(answer)
+    self.answers.where(body: answer).count
+  end
+
 end
